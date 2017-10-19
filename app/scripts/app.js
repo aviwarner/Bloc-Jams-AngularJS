5d546bad8445129afd32b4414de09421654076d2
Avi Warner <aviwarner@gmail.com>
2017-10-19 09:20:17 -0700

    Checkpoint 3 bugfix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,19 +3,23 @@
     $locationProvider
       .html5Mode({
         enabled: true,
-        requiredBase: false
+        requireBase: false
       });
     $stateProvider
       .state('landing', {
         url: '/',
-        templateUrl: 'templates/landing.html'
+        templateUrl: '/templates/landing.html'
       })
       .state('album', {
         url: '/album',
-        templateUrl: 'templates/album.html'
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
   }
   angular
-    .module('blocJams', ['ui-router'])
+    .module('blocJams', ['ui.router'])
     .config(config);
 })();
